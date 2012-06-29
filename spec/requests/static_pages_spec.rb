5e594d5b79453c9ff9dc0423cf8a0fd13aa303e0
mwitek <matt@mwitekdesign.com>
2012-06-29 01:15:59 -0400

    refactored static_pages_spec rspec test to use shared_examples_for, fixed header link and modified .gitignore for .rvmrc

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,29 +1,50 @@
 require 'spec_helper'
 describe "Static pages" do
-let(:base_title){ "Ruby on Rails Tutorial Sample App |"}
- 
+  
   subject {page}
+  
+  before(:all) do
+      @action = puts :action
+  end
+  
+  shared_examples_for 'all static pages' do
+    it {should have_selector('h1', :text => @action)}
+    it {should have_selector('title', :text=>"#{base_title} #{@action}")}
+    it "should have the right links on the layout" do
+      click_link "About"
+      click_link "Help"
+      click_link "Home"
+      click_link "sample app"
+      click_link "Sign up now!"
+      page.should have_selector 'title', :text=>"#{base_title} #{@action}"
+    end 
+  end
+
+  let(:base_title){ "Ruby on Rails Tutorial Sample App |"}
+ 
   describe "Home page" do
     before {visit root_path}
-    it {should have_selector('h1', :text => 'Home')}
-    it {should have_selector('title', :text=>"#{base_title} Home")} 
+    it_should_behave_like 'all static pages'
   end
 
   describe "Help page" do
     before {visit help_path}
-    it { should have_selector('h1', :text=> 'Help')}
-    it { should  have_selector('title', :text => "#{base_title} Help")}
+    it_should_behave_like 'all static pages'
   end
   
   describe "About page" do
     before {visit about_path}
-    it {should have_selector('h1', :text => 'About')}
-    it {should have_selector('title', :text => "#{base_title} About")}
+    it_should_behave_like 'all static pages'
   end
 
   describe "Contact Page" do
     before {visit contact_path}
-  	it {should have_selector('title', :text=>"#{base_title} Contact")}
-  	it {page.should have_selector('h1',:text=>"Contact")}
+  	it_should_behave_like 'all static pages'
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', :text=>"#{base_title} #{@action}"
   end
 end
