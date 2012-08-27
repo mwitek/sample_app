1aedcfbab874a05012767cbba3d4b06bc6c15733
mwitek <matt@mwitekdesign.com>
2012-08-26 23:28:31 -0400

    added relationship model for followers / followed_users

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,6 +20,10 @@ class User < ActiveRecord::Base
 
 	attr_accessible :email, :name, :password, :password_confirmation
 	has_many :microposts, :dependent => :destroy
+	has_many :relationships, :foreign_key => "follower_id", :dependent => :destroy
+	has_many :reverse_relationships, :foreign_key => "followed_id", :class_name =>  "Relationship", :dependent => :destroy
+  has_many :followers, :through => :reverse_relationships, :source => :follower
+	has_many :followed_users, :through => :relationships, :source => :followed
 	
 	has_secure_password
 
@@ -32,6 +36,18 @@ class User < ActiveRecord::Base
 
   def feed
 		Micropost.where("user_id = ?", id)
+	end
+
+	def following?(other_user)
+		relationships.find_by_followed_id(other_user.id)
+	end
+
+	def follow!(other_user)
+		self.relationships.create!(:followed_id => other_user.id)
+	end
+
+	def unfollow!(other_user)
+		relationships.find_by_followed_id(other_user.id).destroy
 	end
 
 	private
